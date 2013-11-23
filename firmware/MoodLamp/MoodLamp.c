94d7dff273338a0d0b9760a451fc718ac75b8bed
Trol <otrifonow@gmail.com>
2013-11-23 16:08:59 +0400

    Minor updates
    code cleanup
    size optimization

--- firmware/MoodLamp/MoodLamp.c
+++ firmware/MoodLamp/MoodLamp.c
@@ -5,14 +5,24 @@
  *  Author: Trol
  */ 
 
+
+#define TIMER_FREQUENCY	9600000
+#define USE_SMALL_RANDOM_GENERATOR	0
+#define TIMER_OVERFLOW_FREQUENCY	(TIMER_FREQUENCY/0xff)
+
 #include <avr/io.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <avr/interrupt.h>
 
+#if !USE_SMALL_RANDOM_GENERATOR
+	#include <stdlib.h>
+#endif
+
+
 uint8_t currentColor[3];
 uint8_t startColor[3];
-uint8_t nextColor[3];
+uint8_t nextColor[3] = {0, 0, 0};
+
 
 #define currentR currentColor[0]
 #define currentG currentColor[1]
@@ -32,43 +42,20 @@ volatile uint8_t rndTime = 50;
 volatile uint8_t rndR = 0;
 volatile uint8_t rndG = 0;
 volatile uint8_t rndB = 150;
-volatile uint8_t rndDarkIndex;	// pregenerated random values
+volatile uint8_t rndDarkIndex;				// pregenerated random values
 volatile bool randomValuesReady = false;
 
 
 
-#define TIMER_FREQUENCY	9600000
-#define TIMER_OVERFLOW_FREQUENCY	(TIMER_FREQUENCY/0xff)
+
 
 
 static uint8_t calculateColor(uint8_t from, uint8_t to);
 static void calcNextPoint();
 
-/*
-uint32_t random() {
-	/
-	 * Compute x = (7^5 * x) mod (2^31 - 1)
-	 * wihout overflowing 31 bits:
-	 *      (2^31 - 1) = 127773 * (7^5) + 2836
-	 * From "Random number generators: good ones are hard to find",
-	 * Park and Miller, Communications of the ACM, vol. 31, no. 10,
-	 * October 1988, p. 1195.
-	 /
-	long hi, lo;
 
-	static long x = 1;
-	// Can't be initialized with 0, so use another value.
-	if (x == 0)
-		x = 123459876L;
-	hi = x / 127773L;
-	lo = x % 127773L;
-	x = 16807L * lo - 2836L * hi;
-	if (x < 0)
-		x += 0x7fffffffL;
-	return x;
-}
-*/
-/*
+
+#if USE_SMALL_RANDOM_GENERATOR
 uint16_t random() {
 	static uint16_t next = 1;
 	uint16_t hi, lo;
@@ -84,7 +71,8 @@ uint16_t random() {
 	}
 	return next;// % max;
 }
-*/
+#endif
+
 
 	
 /** 
@@ -137,7 +125,7 @@ __attribute__ ((noinline)) static uint8_t calculateColor(uint8_t from, uint8_t t
 	return res;
 }
 
-__attribute__ ((noinline)) static uint8_t rndColor() {
+__attribute__ ((noinline)) static uint8_t generateRandomColor() {
 	return random() % 250;
 }
 
@@ -161,15 +149,12 @@ static void calcNextPoint() {
 
 
 int main(void) {
-	nextR = 0;
-	nextG = 0;
-	nextB = 0;
-
 	calcNextPoint();
 	
-	DDRB = 0xff;
 	TCCR0B = _BV(CS00);		// disable prescaler for timer
 	TIMSK0 = _BV(TOIE0);	// enable timer overflow interrupts
+	
+	DDRB = 0xff;
 	sei();					// enable global interrupts
 
     while (1) {
@@ -177,9 +162,9 @@ int main(void) {
 			rndTime = random() % 50;
 			rndTime += 30;					// time to change color, sec/10
 			rndDarkIndex = random() % 3;
-			rndR = rndColor();
-			rndG = rndColor();
-			rndB = rndColor();
+			rndR = generateRandomColor();
+			rndG = generateRandomColor();
+			rndB = generateRandomColor();
 			randomValuesReady = true;
 		}
 	}
--- firmware/write.bat
+++ firmware/write.bat
@@ -1,3 +1,4 @@
-avrdude -c usbasp -pt13 -u -Ulfuse:w:0x7A:m -Uhfuse:w:0xFF:m -Uflash:w:MoodLamp/Release/MoodLamp.hex:a
+rem avrdude -c usbasp -pt13 -u -Ulfuse:w:0x7A:m -Uhfuse:w:0xFF:m -Uflash:w:MoodLamp/Release/MoodLamp.hex:a
+avrdude -c usbasp -pt13 -u -Ulfuse:w:0x7A:m -Uhfuse:w:0xFB:m -Uflash:w:MoodLamp/Release/MoodLamp.hex:a
 
 
